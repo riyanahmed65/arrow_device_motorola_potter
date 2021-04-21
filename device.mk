dd2fbdc5f223d2c09d88e8d4321099cec10490fb
riyanahmed65 <mohd.riyan65@gmail.com>
2021-04-21 15:28:54 +0530

    removed motoaction

--- device.mk
+++ device.mk
@@ -255,8 +255,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.potter
 
 # MotoActions
-PRODUCT_PACKAGES += \
-    MotoActions
+#PRODUCT_PACKAGES += \
+#    MotoActions
+#added MotoAction in android/hardware/MotoAction from lineage os
 
 # Media (OMX)
 PRODUCT_PACKAGES += \
